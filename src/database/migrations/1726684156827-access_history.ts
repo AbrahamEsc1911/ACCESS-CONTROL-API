7b8f770bb07c40ec470c60699b4f3cbe35a89ad7
AEscobarAngola <abrancho1908@gmail.com>
2024-09-23 19:24:56 +0200

    feat: created access history migration

--- src/database/migrations/1726684156827-access_history.ts
+++ src/database/migrations/1726684156827-access_history.ts
@@ -1,11 +1,56 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class AccessHistory1726684156827 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'access_history',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'user_id',
+                    type: 'int'
+                },
+                {
+                    name: 'room_id',
+                    type: 'int',
+                },
+                {
+                    name: 'entry_date',
+                    type: 'datetime',
+                    isNullable: false
+                },
+                {
+                    name: 'exit_date',
+                    type: 'datetime',
+                    isNullable: true
+                }
+            ],
+            foreignKeys: [
+                {
+                    columnNames: ['user_id'],
+                    referencedTableName: 'users',
+                    referencedColumnNames: ['id'],
+                    onDelete: "CASCADE"
+                },
+                {
+                    columnNames: ['room_id'],
+                    referencedTableName: 'rooms',
+                    referencedColumnNames: ['id'],
+                    onDelete: 'CASCADE'
+                }
+            ]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('access_history')
     }
 
 }
