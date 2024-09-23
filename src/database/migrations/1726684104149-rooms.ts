c49a95168cb4141d3e6fa7ce8cee69cac09a41eb
AEscobarAngola <abrancho1908@gmail.com>
2024-09-23 18:52:35 +0200

    feat: creating rooms migration

--- src/database/migrations/1726684104149-rooms.ts
+++ src/database/migrations/1726684104149-rooms.ts
@@ -1,11 +1,41 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Rooms1726684104149 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'rooms',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'room',
+                    type: 'varchar',
+                    length: '100',
+                    isNullable: false,
+                },
+                {
+                    name: 'capacity',
+                    type: 'int',
+                    isNullable: false
+                },
+                {
+                    name: 'room type',
+                    type: 'enum',
+                    enum: ['meetings', 'office'],
+                    isNullable: false,
+                }
+            ]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('rooms')
     }
 
 }
