5554ec0e3f0cb9f9b9e161bd00c42e008f7e9e95
AEscobarAngola <abrancho1908@gmail.com>
2024-09-28 17:16:40 +0200

    feat: creating controller to create a new admin

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -66,6 +66,70 @@ export const register = async (req: Request, res: Response) => {
     }
 }
 
+export const registerAdmin = async (req: Request, res: Response) => {
+    try {
+
+        const { email, name, password, StartUp, dni, phone } = req.body
+
+        if (!email || !password || !StartUp || !dni || !phone) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: 'All values are requiered'
+                }
+            )
+        }
+
+        if (password.length < 8 || password.length > 12) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: 'The password must be between 8 and 12 characters long'
+                }
+            )
+        }
+        if (dni.length !== 9) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: 'check DNI'
+                }
+            )
+        }
+
+        const passHashed = bcrypt.hashSync(password, 10)
+
+       const newAdmin = await Users.create(
+        {
+            name: name,
+            email: email,
+            password: passHashed,
+            StartUp: StartUp,
+            dni: dni,
+            phone: phone,
+            role: 'admin'
+        }
+       ).save()
+
+       const { password: pass, ...userWithoutPassword } = newAdmin;
+
+       res.json({
+        success: true,
+        message: 'admin created',
+        data: userWithoutPassword
+       })
+
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message: 'Internal Error creating new admin',
+                error: error
+            }
+        )
+    }
+}
+
 export const login = async (req: Request, res: Response) => {
     try {
 
--- src/routers/auth.routers.ts
+++ src/routers/auth.routers.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, register } from "../controllers/authentication.controller";
+import { login, register, registerAdmin } from "../controllers/authentication.controller";
 
 const router = Router()
 
 router.post('/register', register)
+router.post('/admin', registerAdmin)
 router.post('/login', login)
 
 export { router }
