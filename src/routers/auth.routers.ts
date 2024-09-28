634e9260c5a26d866f18692e8f55431b96cf62a2
AEscobarAngola <abrancho1908@gmail.com>
2024-09-28 19:49:52 +0200

    feat: adding middlewares to currents endpoints

--- src/routers/auth.routers.ts
+++ src/routers/auth.routers.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { login, register, registerAdmin } from "../controllers/authentication.controller";
+import { auth } from "../middlewares/auth";
+import { isAdmin } from "../middlewares/isAdmin";
 
 const router = Router()
 
 router.post('/register', register)
-router.post('/admin', registerAdmin)
+router.post('/admin', auth, isAdmin, registerAdmin)
 router.post('/login', login)
 
 export { router }
--- src/routers/rooms.routers.ts
+++ src/routers/rooms.routers.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createNewRoom, deleteRoomById, getAllRooms, updateRoom } from "../controllers/rooms.controller";
+import { auth } from "../middlewares/auth";
+import { isAdmin } from "../middlewares/isAdmin";
 
 const router = Router()
 
 router.get('/', getAllRooms)
-router.post('/', createNewRoom)
-router.put('/:id', updateRoom)
-router.delete('/:id', deleteRoomById)
+router.post('/', auth, isAdmin, createNewRoom)
+router.put('/:id', auth, isAdmin, updateRoom)
+router.delete('/:id', auth, isAdmin, deleteRoomById)
 
 export { router }
