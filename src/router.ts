d551d92a2ed9ab677c41d5653937d1db5bc812fb
AEscobarAngola <abrancho1908@gmail.com>
2024-09-28 17:19:41 +0200

    feat: new router file created for rooms

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { router as auhtRouters } from "./routers/auth.routers"
+import { router as roomRouter } from "./routers/rooms.routers"
 
 const router = Router()
 
 router.use('/auth', auhtRouters)
+router.use('/rooms', roomRouter)
 
 export { router }
