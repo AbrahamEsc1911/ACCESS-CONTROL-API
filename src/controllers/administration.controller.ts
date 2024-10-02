1002134dcc9f181becf63eee3881ae5238f32fd4
AEscobarAngola <abrancho1908@gmail.com>
2024-10-02 19:36:39 +0200

    feat: repors by dates, controller created and routers

--- src/controllers/administration.controller.ts
+++ src/controllers/administration.controller.ts
@@ -21,18 +21,18 @@ export const dailyReport = async (req: Request, res: Response) => {
             }
         )
 
-        for(const reservation of reservations) {
+        for (const reservation of reservations) {
             const entryDate = new Date(reservation.entry_date)
 
             if (reservation.state === 'reserved') {
                 const differenceInMs = currentTime.getTime() - entryDate.getTime()
                 const differenceInMins = differenceInMs / (1000 * 60)
 
-                if(differenceInMins > timeLimitInMins) {
+                if (differenceInMins > timeLimitInMins) {
                     await Access.update(
                         {
                             id: reservation.id
-                        }, 
+                        },
                         {
                             state: 'no_show'
                         }
@@ -49,7 +49,7 @@ export const dailyReport = async (req: Request, res: Response) => {
             }
         )
 
-        if(dailyReport.length == 0) {
+        if (dailyReport.length == 0) {
             return res.status(404).json(
                 {
                     success: false,
@@ -71,6 +71,90 @@ export const dailyReport = async (req: Request, res: Response) => {
             {
                 success: false,
                 message: 'Internar error genereting a daily report',
+                error: error
+            }
+        )
+    }
+}
+
+export const report = async (req: Request, res: Response) => {
+    try {
+        const { start_date, end_date } = req.query
+
+        if (!start_date || !end_date) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: 'start date and end date are required'
+                }
+            )
+        }
+
+        const startDate = new Date(String(start_date))
+        const endDate = new Date(String(end_date))
+
+        if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: 'Invalid date format, Please use YYYY-MM-DD format'
+                }
+            )
+        }
+
+        startDate.setHours(0, 0, 0, 0);
+        endDate.setHours(23, 59, 59, 999);
+
+        const currentTime = new Date()
+        const timeLimitInMins = 30
+
+        const reservations = await Access.find(
+            {
+                where: {
+                    entry_date: Between(startDate, endDate)
+                }
+            }
+        )
+
+        for (const reservation of reservations) {
+            const entryDate = new Date(reservation.entry_date)
+
+            if (reservation.state === 'reserved') {
+                const differenceInMs = currentTime.getTime() - entryDate.getTime()
+                const differenceInMins = differenceInMs / (1000 * 60)
+
+                if (differenceInMins > timeLimitInMins) {
+                    await Access.update(
+                        {
+                            id: reservation.id
+                        },
+                        {
+                            state: 'no_show'
+                        }
+                    )
+                }
+            }
+        }
+
+        const report = await Access.find(
+            {
+                where: {
+                    entry_date: Between(startDate, endDate)
+                }
+            }
+        )
+
+        return res.status(200).json({
+            success: true,
+            message: 'Showing a report for the given date range.',
+            data: report,
+        });
+
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message: 'internal error retriving report',
                 error: error
             }
         )
--- src/routers/administration.routers.ts
+++ src/routers/administration.routers.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { dailyReport } from "../controllers/administration.controller"
+import { dailyReport, report } from "../controllers/administration.controller"
 import { auth } from "../middlewares/auth"
 import { isAdmin } from "../middlewares/isAdmin"
 
 const router = Router()
 
 router.get('/daily-report', auth, isAdmin, dailyReport)
+router.get('/reports', auth, isAdmin, report)
 
 export {router}
