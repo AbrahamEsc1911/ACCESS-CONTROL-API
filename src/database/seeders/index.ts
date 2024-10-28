dcda45088ac56c1c9322326c791943e5d2454b5c
AEscobarAngola <abrancho1908@gmail.com>
2024-10-28 22:29:58 +0100

    update: user seader correction

--- src/database/seeders/index.ts
+++ src/database/seeders/index.ts
@@ -1,7 +1,9 @@
 import { accessSeeders } from "./accessSeeders";
+import { userSeeders } from "./userSeeders";
 
 (async () => {
   console.log('starting seeders...');
-  await accessSeeders()
+  await userSeeders()
+  // await accessSeeders()
   console.log('finishing seeders...');
 })()
--- src/database/seeders/userSeeders.ts
+++ src/database/seeders/userSeeders.ts
@@ -1,43 +1,82 @@
+import { AppDataSource } from "../db";
+import { Users } from "../models/Users";
+import bcrypt from 'bcrypt'
 
-const { Users } = require('../models/Users');
-
-const users = [
-  {
-    name: 'John Doe',
-    StartUp: 'Tech Innovators',
-    email: 'user@user.com',
-    password: 'password', 
-    dni: '12345678A',
-    phone: 1234567890,
-    role: 'user', 
-  },
-  {
-    name: 'Jane Smith',
-    StartUp: 'Creative Agency',
-    email: 'admin@admin.com',
-    password: 'password', 
-    dni: '87654321B',
-    phone: 9876543210,
-    role: 'admin', 
-  },
-  {
-    name: 'Alice Johnson',
-    StartUp: 'Design Studio',
-    email: 'alice@example.com',
-    password: 'password', 
-    dni: '23456789C',
-    phone: 1112223333,
-    role: 'user', 
-  },
-];
-
-const userSeeder = async () => {
+export const userSeeders = async () => {
   try {
-    await Users.bulkCreate(users);
-    console.log('Users seeded successfully!');
-  } catch (error) {
-    console.error('Error seeding users:', error);
-  }
-};
 
-module.exports = { userSeeder };
+    await AppDataSource.initialize()
+
+    const users = [
+      {
+        name: 'John Doe',
+        StartUp: 'Tech Innovators',
+        email: 'user@user.com',
+        password: 'password',
+        dni: '12345678A',
+        phone: 1234567890,
+        role: 'user',
+      },
+      {
+        name: 'Jane Smith',
+        StartUp: 'Creative Agency',
+        email: 'admin@admin.com',
+        password: 'password',
+        dni: '87654321B',
+        phone: 9876543210,
+        role: 'admin',
+      },
+      {
+        name: 'Alice Johnson',
+        StartUp: 'Design Studio',
+        email: 'alice@example.com',
+        password: 'password',
+        dni: '23456789C',
+        phone: 1112223333,
+        role: 'user',
+      },
+    ];
+
+    const newUsers = await createUsers(users)
+
+    await Users.save(newUsers)
+
+    console.log('===========================');
+    console.log('Users seeder successfully');
+    console.log('===========================');
+
+  } catch (error: any) {
+
+    console.log('----------------')
+    console.log(`ERROR USERS SEEDERS ${error.message}`)
+    console.log('----------------')
+
+  } finally {
+
+    AppDataSource.destroy()
+
+  }
+}
+
+const createUsers = async (arr: object[]) => {
+  const newUser: Users[] = []
+
+  arr.forEach((element: any, index: any) => {
+
+    const passHashed = bcrypt.hashSync(element.password, 10)
+
+    const user = new Users()
+    user.id = index + 1
+    user.name = element.name
+    user.StartUp = element.StartUp
+    user.email = element.email
+    user.password = passHashed
+    user.dni = element.dni
+    user.phone = element.phone
+    user.role = element.role
+    newUser.push(user)
+
+  })
+
+  return newUser;
+}
