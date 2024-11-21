a371b8cdf2d12cf6bd13610cd76f0d947247197a
AEscobarAngola <abrancho1908@gmail.com>
2024-11-21 21:37:43 +0100

    fix: correction on seeder access histories, changed all estructure of code

--- src/database/seeders/accessHistorySeeders.ts
+++ src/database/seeders/accessHistorySeeders.ts
@@ -1,33 +1,61 @@
-const { AccessHistory } = require('../database/models/AccessHistory'); 
+import { AppDataSource } from "../db";
+import { AccessHistory } from "../models/AccessHistory";
 
-const accessHistories = [
-  {
-    user_id: 1, 
-    room_id: 1, 
-    entry_date: new Date('2024-10-17T09:00:00Z'),
-    exit_date: new Date('2024-10-17T10:00:00Z'),
-  },
-  {
-    user_id: 2, 
-    room_id: 2, 
-    entry_date: new Date('2024-10-17T11:00:00Z'),
-    exit_date: new Date('2024-10-17T12:30:00Z'),
-  },
-  {
-    user_id: 3, 
-    room_id: 3,
-    entry_date: new Date('2024-10-17T13:00:00Z'),
-    exit_date: new Date('2024-10-17T15:00:00Z'),
-  },
-];
-
-const seedAccessHistories = async () => {
+export const accessHistories = async () => {
   try {
-    await AccessHistory.bulkCreate(accessHistories);
-    console.log('Access Histories seeded successfully!');
-  } catch (error) {
-    console.error('Error seeding access histories:', error);
-  }
-};
+    await AppDataSource.initialize()
 
-module.exports = { seedAccessHistories };
+    const accessHistories = [
+      {
+        user_id: 1,
+        room_id: 1,
+        entry_date: new Date('2024-10-17T09:00:00Z'),
+        exit_date: new Date('2024-10-17T10:00:00Z'),
+      },
+      {
+        user_id: 2,
+        room_id: 2,
+        entry_date: new Date('2024-10-17T11:00:00Z'),
+        exit_date: new Date('2024-10-17T12:30:00Z'),
+      },
+      {
+        user_id: 3,
+        room_id: 3,
+        entry_date: new Date('2024-10-17T13:00:00Z'),
+        exit_date: new Date('2024-10-17T15:00:00Z'),
+      },
+    ];
+
+    const newAccesshistories = await createHistories(accessHistories)
+
+    await AccessHistory.save(newAccesshistories)
+
+    console.log('===========================');
+    console.log('Access Histories seeder successfully');
+    console.log('===========================');
+
+  } catch (error: any) {
+
+    console.log('----------------')
+    console.log(`ERROR ACCESS HISTORIES SEEDERS ${error.message}`)
+    console.log('----------------')
+
+  } finally {
+    await AppDataSource.destroy()
+  }
+}
+
+const createHistories = async (arr: object[]) => {
+  const newAccesshistories: AccessHistory[] = []
+
+  arr.forEach((element: any) => {
+    const history = new AccessHistory()
+    history.user_id = element.user_id
+    history.room_id = element.room_id
+    history.entry_date = element.entry_date
+    history.exit_date = element.exit_date
+    newAccesshistories.push(history)
+  })
+
+  return newAccesshistories
+}
